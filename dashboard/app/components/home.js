7d7a7fc5639f8753a4ee660a73723319b4628120
Lucas Menendez <epucas@gmail.com>
2018-01-18 15:46:13 +0100

    texterror

--- dashboard/app/components/home.js
+++ dashboard/app/components/home.js
@@ -1,6 +1,6 @@
 let Home = Vue.component("home-component", {
 	template:	`<div class="jumbotron">
-					<h2 class="display-8">Wellcome to LabsWeb Dashboard</h2>
+					<h2 class="display-8">Welcome to LabsWeb Dashboard</h2>
 					<p class="lead">If you are here, it's because you are <b>one of the chosen ones</b> to innovate.</p>
 					<p class="lead">Browse through resources to create new ones or edit them.</p>
 				</div>`
