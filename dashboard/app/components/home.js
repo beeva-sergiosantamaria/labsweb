58124634974c84567c4a0b4756a5d3b305380ed0
Lucas Menendez <epucas@gmail.com>
2018-01-19 20:23:52 +0100

    Scrollbar issue on login page fixed

--- dashboard/app/components/home.js
+++ dashboard/app/components/home.js
@@ -1,7 +1,9 @@
 let Home = Vue.component("home-component", {
-	template:	`<div class="jumbotron">
-					<h2 class="display-8">Welcome to LabsWeb Dashboard</h2>
-					<p class="lead">If you are here, it's because you are <b>one of the chosen ones</b> to innovate.</p>
-					<p class="lead">Browse through resources to create new ones or edit them.</p>
+	template:	`<div class="card">
+					<div class="card-body pw-0 pb-2">
+						<h4 class="card-title">Welcome to LabsWeb Dashboard</h4>
+						<p class="card-text">If you are here, it's because you are <b>one of the chosen ones</b> to innovate.</p>
+						<p class="card-text">Browse through resources to create new ones or edit them.</p>
+					</div>
 				</div>`
 });
--- dashboard/app/components/login.js
+++ dashboard/app/components/login.js
@@ -1,17 +1,19 @@
 let Login = Vue.component("login-component", {
-	template:	`<div class="h-100 d-flex justify-content-center">
-					<div class="row align-self-center justify-content-center">
-						<div class="col-5 text-center">
-							<div class="card my-5">
-								<div class="card-header">
-									<img class="w-75" src="assets/img/logo.png"/>
-								</div>
-								<div class="card-body">
-									<h5 class="card-title">LabsWeb Dashboard</h5>
-									<p class="card-text">Manage all resources of LabsWeb.</p>
-									<button type="button" class="btn btn-primary" v-on:click="signIn">
-										Login with <i class="fa fa-lg fa-google"></i>
-									</button>
+	template:	`<div class="container">
+					<div class="h-100 d-flex justify-content-center">
+						<div class="row align-self-center justify-content-center">
+							<div class="col-5 text-center">
+								<div class="card my-5">
+									<div class="card-header">
+										<img class="w-75" src="assets/img/logo.png"/>
+									</div>
+									<div class="card-body">
+										<h5 class="card-title">LabsWeb Dashboard</h5>
+										<p class="card-text">Manage all resources of LabsWeb.</p>
+										<button type="button" class="btn btn-primary" v-on:click="signIn">
+											Login with <i class="fa fa-lg fa-google"></i>
+										</button>
+									</div>
 								</div>
 							</div>
 						</div>
